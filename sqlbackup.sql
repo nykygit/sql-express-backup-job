db218e88186fa208d5b75a5aee1447ecc5be55b5
nykygit <18310180+nykygit@users.noreply.github.com>
2020-06-10 11:51:31 -0500

    Update sqlbackup.sql

--- sqlbackup.sql
+++ sqlbackup.sql
@@ -16,6 +16,10 @@ RETENTION OPTIONS
 2. 1 BAK file for each database + each day of the week - dbName_Monday.BAK, dbname_Tuesday.BAK, etc.. (schedule this daily)
 3. 1 BAK file for each database + each day of the month - dbName_1.BAK, dbName_2.BAK, etc...to 31.BAK (schedule this daily)
 4. 1 BAK file for each week of the year (schedule this as often as you want, eg daily, but at least once a week)
+5. 1 BAK file for each month of the year (schedule this as often as you want, but at least once a month to retain monthly)
+6. Classic GFS setup, daily for 1 week, weekly for 1 month, monthly for 1 year, 1 end of year, never overwrite...you will run out of disk space eventually
+
+Note - Retention is achieved by Overwriting files, not by Deletes.
 */
 
 /* ========================================
